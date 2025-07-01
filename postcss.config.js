4bb1877779d7cc1194a89086f0218b68fe84c0b6
so-hay <hay.soraka@gmail.com>
2025-07-01 17:33:37 +0900

    ３回目修正

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
